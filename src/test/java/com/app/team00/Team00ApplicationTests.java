43ea7d96155c542956654a75ae8ee2b418bebf2b
dev.tedhan <dev.tedhan@gmail.com>
2026-02-04 13:44:56 +0900

    init

---
+++ src/test/java/com/app/team00/Team00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.team00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
